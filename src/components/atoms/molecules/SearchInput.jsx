5ff3048f45df011d4816f665c1fbb12e8db4c548
Shoma-nam <132134861+Shoma-nam@users.noreply.github.com>
2023-07-10 23:31:01 +0900

    Initial commit

---
+++ src/components/atoms/molecules/SearchInput.jsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
+export const SeachInput = () => {
+  return (
+    <div>
+      <input />
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+};
