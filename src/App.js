702572d4e92226514626f33b9b961bb6a31d8717
Shoma <132134861+Shoma-nam@users.noreply.github.com>
2023-07-24 11:34:32 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "アーセナル",
-  image: "https://source.unsplash.com/aC-Qup8UkQw",
-  email: "12345@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
 
 export default function App() {
   return <Router />;
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,11 +1,33 @@
 import styled from "styled-components";
 import { SearchInput } from "../atoms/molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+    return {
+    id: val,
+    name: `アーセナル${val}`,
+    image: "https://source.unsplash.com/aC-Qup8UkQw",
+    email: "12345@example.com",
+    phone: "090-1111-2222",
+    company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+        
+    };
+});
+
 
 export const Users = () => {
   return (
     <SContainer>
+      <SUserArea>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user}/>
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
@@ -14,5 +36,13 @@ const SContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  pading: 24px;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+padding-top: 40px;
+width: 100%;
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+grid-gap: 20px;
 `;
